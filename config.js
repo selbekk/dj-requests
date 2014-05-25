43ee8203f4fb0ee80d50854e630f9f9c936614e2
selbekk <kristofer.selbekk@bekk.no>
2014-05-25 18:30:42 +0200

    Refactor config file

--- config.js
+++ config.js
@@ -1,15 +1,26 @@
-var bodyParser = require('body-parser');
 var express = require('express');
 var path = require('path');
 var Logger = require('basic-logger');
 
 module.exports = {
-	setup: function(app) {
-		Logger.setLevel('trace');
-
-		app.use(bodyParser());
-		app.use(express.static(path.join(__dirname, 'public')));
-	}
+        setup: function(app) {
+                setup(app);
+                setupErrorHandling(app);
+        }
 };
 
+var setupErrorHandling = function(app) {
+	/// catch 404 and forwarding to error handler
+	app.use(function(req, res, next) {
+    		var err = new Error('Not Found');
+    		err.status = 404;
+    		next(err);
+	});
+};
+
+var setup = function(app) {
+	Logger.setLevel('trace');
+
+        app.use(express.static(path.join(__dirname, 'public')));
+};
 
